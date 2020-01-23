821f7f6f240bddec239035f905e8ff2aad1f7dd8
David Mitjana Castro <d.mitjana@gmail.com>
2020-01-24 00:08:41 +0100

    display comments

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -5,21 +5,42 @@ import commentsService from './../services/comments-service'
 
 class CommentsList extends Component {
 
+  state = {
+    comments: []
+  }
   componentDidMount() {
     const { id } = this.props.exerciseId;
     console.log(id);
     
     commentsService.getAll(id)
-      .then( allComments => {
-        console.log(allComments.comments)
+      .then( comments => {
+        console.log(comments)
+        this.setState({comments})
       })
       .catch(err => console.log(err));
   }
 
   render() {
+    const comments = this.state.comments;
     return (
       <div>
-
+        {
+          comments
+          ? 
+          <div>
+            {
+              comments.map(comment => {
+                return (
+                  <div>
+                    <p>{comment.text}</p>
+                    <strong>{comment.author.name} {comment.author.lastName}</strong>
+                  </div>
+                )
+              })
+            }
+          </div>
+          : <p>No comments</p>
+        }
       </div>
     )
   }
