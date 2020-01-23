235c3afc5b91b6e03a2267bb0da4a02f3ab9f83b
David Mitjana Castro <d.mitjana@gmail.com>
2020-01-23 23:34:06 +0100

    add comments service

---
+++ src/services/comments-service.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+class Comments {
+  constructor() {
+    this.comments = axios.create({
+      baseURL: process.env.REACT_APP_API_URL + '/comments',
+      withCredentials: true,
+    })
+  }
+
+  // get all comments from one exercise id
+  getAll(exerciseId) {
+    return this.comments
+              .get(`/${exerciseId}`)
+              .then(({ data }) => data);
+  }
+
+  // post a comment to an exercise id
+  create(exerciseId) {
+    return this.comments
+              .post(`${exerciseId}`)
+              .then(({data}) => data);
+  }
+
+  // delete comment from exercise id
+  deleteOne(exerciseId, commentId) {
+    return this.comments
+              .delete(`/${exerciseId}/${commentId}`)
+              .then(({ data }) => data);
+  }
+
+  // edit-update comment
+
+}
+
+const commentsService = new Comments();
+
+export default commentsService;
