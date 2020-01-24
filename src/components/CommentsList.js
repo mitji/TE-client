225e0f2571b51b5e50c6551bae91ce9ee23ab76a
David Mitjana Castro <d.mitjana@gmail.com>
2020-01-24 12:06:01 +0100

    add styles to comment

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 import commentsService from './../services/comments-service'
+import './../styles/comments.scss';
 
 class CommentsList extends Component {
 
   state = {
     comments: []
   }
+
+  parseDate = (date) => {
+    const dateToParse= date.slice(0,date.indexOf('T'));
+    const dateStr = dateToParse.split('-');
+    const d = new Date(dateStr);
+    const day = d.getDate();
+    const month = d.toLocaleString('default', { month: 'long' });
+    const year = d.getFullYear();
+    return {day, month, year};
+  }
+
   componentDidMount() {
     const { id } = this.props.exerciseId;
-    console.log(id);
     
     commentsService.getAll(id)
       .then( comments => {
-        console.log(comments)
         this.setState({comments})
       })
       .catch(err => console.log(err));
@@ -24,16 +34,19 @@ class CommentsList extends Component {
     const comments = this.state.comments;
     return (
       <div>
+        <h4>Comments</h4>
         {
           comments
           ? 
           <div>
             {
               comments.map(comment => {
+                const { day, month, year } = this.parseDate(comment.created_at);
                 return (
-                  <div>
-                    <p>{comment.text}</p>
-                    <strong>{comment.author.name} {comment.author.lastName}</strong>
+                  <div className="comment">
+                    <strong className="comment__author">{comment.author.name} {comment.author.lastName}</strong>
+                    <p className="comment__date">{month} {day}, {year}</p>
+                    <p className="comment__text">{comment.text}</p>
                   </div>
                 )
               })
